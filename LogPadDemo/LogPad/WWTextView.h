9097dd11814d2c7c152ba3ff8919335da481bb75
morefine <990802260@qq.com>
2019-07-24 09:50:59 +0800

    修改文本显示控件

--- LogPadDemo/LogPad/WWTextView.h
+++ LogPadDemo/LogPad/WWTextView.h
@@ -3,10 +3,20 @@
 //  XJKHealth
 //
 //  Created by wangwei on 2018/7/26.
-//  Copyright © 2018年 WW. All rights reserved.
+//  Copyright © 2018年 xiaweidong. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
+typedef NS_ENUM(NSUInteger ,limitTextType) {
+    DEFAULT        = 0, //(0/100)
+    TEXT_SERIAL    = 1  //(还可以输入0个字)
+};
+@class WWTextView;
+@protocol WWTextViewDelegate <NSObject>
+@optional
+-(void)textViewBeginEditing:(WWTextView *)textView;
+-(void)textView:(WWTextView *)textView textDidChange:(NSString *)text adjustHeight:(CGFloat) height;
+@end
 
 @interface WWTextView : UIView
 @property (nonatomic, copy) NSString *text;               //textView的文本内容
@@ -25,8 +35,11 @@
 @property (nonatomic, strong) UIColor  *limitTextColor;   //限制显示文本颜色
 @property (nonatomic, strong) UIFont   *limitFont;        //限制显示文本字号
 @property (nonatomic, assign) NSInteger limitLength;      //不设置输入限制长度时，textView的下部分限制显示文本就不会出现
+@property (nonatomic, assign) limitTextType limitTextType;       //限制文本显示格式
 @property (nonatomic, assign) BOOL limitLabelHidden;      //textView的下部分限制显示文本是否显示（显示：false,隐藏：true）
-
+@property (nonatomic, weak) id<WWTextViewDelegate> delegate;
+@property (nonatomic, assign) NSInteger maxTextH;         //最大文本高度
+@property (nonatomic, assign) BOOL enableEdit;            //textView是否可编辑（默认为true）
 -(instancetype)init NS_UNAVAILABLE;
 /**
  设置边框
--- LogPadDemo/LogPad/WWTextView.m
+++ LogPadDemo/LogPad/WWTextView.m
@@ -3,7 +3,7 @@
 //  XJKHealth
 //
 //  Created by wangwei on 2018/7/26.
-//  Copyright © 2018年 WW. All rights reserved.
+//  Copyright © 2018年 xiaweidong. All rights reserved.
 //
 
 #import "WWTextView.h"
@@ -36,12 +36,17 @@
     self.textView.delegate = self;
     self.textView.returnKeyType = UIReturnKeyDone;
     self.textView.backgroundColor = [UIColor clearColor];
+    self.textView.scrollEnabled = self.maxTextH > 0  ? false : true;
+    self.enableEdit = true;
     [self addSubview:self.textView];
     
     self.limitLabel = [[UILabel alloc] init];
+    self.limitFont = [UIFont systemFontOfSize:13.0];
+    self.limitTextColor = [UIColor lightGrayColor];
     self.limitLabel.numberOfLines = 0;
     self.limitLabel.textAlignment = NSTextAlignmentRight;
-    [self.limitLabel sizeToFit];
+    self.limitLabel.adjustsFontSizeToFitWidth = true;
+    self.limitTextType = DEFAULT;
     [self addSubview:self.limitLabel];
 }
 -(void)addViews{
@@ -60,6 +65,9 @@
         self.textView.frame = CGRectMake(0, 0, self.frame.size.width, self.frame.size.height);
         self.limitLabel.hidden = true;
     }
+    if (_limitLength){
+        self.limitLength = _limitLength;//主要是再做一次布局
+    }
 }
 -(NSString *)text{
     return self.textView.text;
@@ -68,6 +76,19 @@
     //不将赋值放到主队列的最后，会导致textView有初始化内容时placeholderLabel还是会显示出来
     dispatch_async(dispatch_get_main_queue(), ^{
         self.textView.text = text;
+        if (((NSInteger)text.length + text.length)>= self.limitLength){
+            if (self.limitTextType == DEFAULT){
+                self.limitLabel.text = [NSString stringWithFormat:@"%d/%ld",0,self->_limitLength];
+            } else {
+                self.limitLabel.text = @"还可以输入0个字";
+            }
+        } else {
+            if (self.limitTextType == DEFAULT){
+                self.limitLabel.text = [NSString stringWithFormat:@"%ld/%ld",self.limitLength - ((NSInteger)text.length + text.length),self->_limitLength];
+            } else {
+                self.limitLabel.text = [NSString stringWithFormat:@"还可以输入%ld个字",self.limitLength - ((NSInteger)text.length + text.length)];
+            }
+        }
     });
 }
 -(void)setAttributedText:(NSAttributedString *)attributedText{
@@ -94,15 +115,29 @@
 }
 -(void)setLimitLength:(NSInteger)limitLength{
     _limitLength = limitLength;
-    self.limitLabel.text = [NSString stringWithFormat:@"还可以输入%ld个字",limitLength];
+    if (_limitTextType == DEFAULT){
+        self.limitLabel.text = [NSString stringWithFormat:@"%ld/%ld",limitLength,limitLength];
+    } else {
+        self.limitLabel.text = [NSString stringWithFormat:@"还可以输入%ld个字",limitLength];
+    }
     CGSize labelSize = [self.limitLabel sizeThatFits:CGSizeMake(200, 30)];
-    self.limitLabel.frame = CGRectMake(self.frame.size.width - 10 - labelSize.width - 20, self.frame.size.height - labelSize.height, labelSize.width + 20, labelSize.height);
+    self.limitLabel.frame = CGRectMake(self.frame.size.width - 10 - labelSize.width, self.frame.size.height - labelSize.height, labelSize.width, labelSize.height);
     //重新设置frame某一个空间的frame都会调用layoutSubviews方法
     self.textView.frame = CGRectMake(0, 0, self.frame.size.width, self.frame.size.height - self.limitLabel.frame.size.height);
+}
+-(void)setLimitTextType:(limitTextType)limitTextType{
+    _limitTextType = limitTextType;
+    [self setLimitLength:_limitLength];
 }
 -(void)setLimitLabelHidden:(BOOL)limitLabelHidden{
     _limitLabelHidden = limitLabelHidden;
     [self setNeedsLayout];
+}
+-(BOOL)textViewShouldBeginEditing:(UITextView *)textView{
+    if (self.delegate && [self.delegate respondsToSelector:@selector(textViewBeginEditing:)]){
+        [self.delegate textViewBeginEditing:self];
+    }
+    return self.enableEdit;
 }
 -(BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text{
     if ([text isEqualToString:@"\n"]){
@@ -113,15 +148,37 @@
         return false;
     }
     if ([text isEqualToString:@""]){
-        self.limitLabel.text = [NSString stringWithFormat:@"还可以输入%ld个字",self.limitLength - ((NSInteger)textView.text.length - 1 != 0 && (NSInteger)textView.text.length - 1 > 0 ? (NSInteger)textView.text.length - 1 : 0)];
+        if (self.limitTextType == DEFAULT){
+             self.limitLabel.text = [NSString stringWithFormat:@"%ld/%ld",self.limitLength - ((NSInteger)textView.text.length - 1 != 0 && (NSInteger)textView.text.length - 1 > 0 ? (NSInteger)textView.text.length - 1 : 0),_limitLength];
+        } else {
+            self.limitLabel.text = [NSString stringWithFormat:@"还可以输入%ld个字",self.limitLength - ((NSInteger)textView.text.length - 1 != 0 && (NSInteger)textView.text.length - 1 > 0 ? (NSInteger)textView.text.length - 1 : 0)];
+        }
     } else {
         if (((NSInteger)textView.text.length + text.length)>= self.limitLength){
-            self.limitLabel.text = @"还可以输入0个字";
+            if (self.limitTextType == DEFAULT){
+                self.limitLabel.text = [NSString stringWithFormat:@"%d/%ld",0,_limitLength];
+            } else {
+                self.limitLabel.text = @"还可以输入0个字";
+            }
         } else {
-            self.limitLabel.text = [NSString stringWithFormat:@"还可以输入%ld个字",self.limitLength - ((NSInteger)textView.text.length + text.length)];
+            if (self.limitTextType == DEFAULT){
+                 self.limitLabel.text = [NSString stringWithFormat:@"%ld/%ld",self.limitLength - ((NSInteger)textView.text.length + text.length),_limitLength];
+            } else {
+                 self.limitLabel.text = [NSString stringWithFormat:@"还可以输入%ld个字",self.limitLength - ((NSInteger)textView.text.length + text.length)];
+            }
         }
     }
     return true;
+}
+-(void)textViewDidChange:(UITextView *)textView{
+    if (self.delegate && [self.delegate respondsToSelector:@selector(textView:textDidChange:adjustHeight:)]){
+        NSInteger height = ceilf([self wwSizeThatFits:CGSizeMake(self.bounds.size.width, MAXFLOAT)].height);
+        if (height > self.maxTextH && self.maxTextH){
+            self.textView.scrollEnabled = true;
+            height = self.maxTextH;
+        }
+        [self.delegate textView:self textDidChange:textView.text adjustHeight:height];
+    }
 }
 //设置边框颜色
 - (void)setBorderWidth:(float)borderWidth withColor:(UIColor *)color{
@@ -147,5 +204,11 @@
 -(void)setScrollEnabled:(BOOL)scrollEnabled{
     _scrollEnabled = scrollEnabled;
     self.textView.scrollEnabled = scrollEnabled;
+}
+-(CGSize)wwSizeThatFits:(CGSize)size{
+    return [self.textView sizeThatFits:size];
+}
+-(void)wwLayout{
+    [self setNeedsLayout];
 }
 @end
